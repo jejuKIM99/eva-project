9316f26c33bd67666aaa0432acf431de95400b49
KimHS <kkspooto@gmail.com>
2025-07-01 12:05:54 +0900

    .

--- src/components/MenuCarousel.js
+++ src/components/MenuCarousel.js
@@ -33,7 +33,7 @@ const MenuCarousel = ({ onActiveItemClick, initialDelay }) => {
         const calculateWidths = () => {
             const currentWidth = window.innerWidth;
             if (currentWidth <= 768) {
-                setItemWidth(156.6);
+                setItemWidth(156);
                 setViewportWidth(currentWidth);
             } else if (currentWidth <= 1024) {
                 setItemWidth(220);
