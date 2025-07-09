a0ddd38c05c02d6c62d66305acfdb859047c4f53
KimHS <kkspooto@gmail.com>
2025-07-09 15:09:26 +0900

    Create cojuscan.js

---
+++ public/cojuscan/cojuscan.js
@@ -0,0 +1,14 @@
+// cojuscan.js
+import './src/components/AngelsPage.js';
+import './src/components/CreditsPage.js';
+import './src/components/EvangelionPage.js';
+import './src/components/GuestbookPage.js';
+import './src/components/LCLPage.js';
+import './src/components/MainContent.js';
+import './src/components/MenuCarousel.js';
+import './src/components/NervPage.js';
+import './src/components/PilotsPage.js';
+import './src/components/SecondImpactPage.js';
+import './src/components/SeelePage.js';
+import './src/components/S2EnginePage.js';
+
